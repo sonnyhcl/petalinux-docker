776d84d28ac56542e5f952a9fffa027059297904
sonnyhcl <sonnyhcl@163.com>
2018-12-10 23:49:37 +0800

    petalinux:2018.2 draft-v0.1 15.5G

--- 2018.2/auto-install.sh
+++ 2018.2/auto-install.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env expect
 set timeout -1
 set install_dir [lindex $argv 0]
-set petalinux_version [lindex $argv 1]
+set version [lindex $argv 1]
 
-spawn ./petalinux-v${petalinux_version}-final-installer.run $install_dir
+send_user "install dir is $install_dir\n"
+send_user "version is $version\n"
+
+spawn ./petalinux-v$version-final-installer.run $install_dir
 expect "Press Enter to display the license agreements"
 send "\r"
+expect "*>*"
+send "y\r"
 expect "*>*"
 send "y\r"
 expect "*>*"
